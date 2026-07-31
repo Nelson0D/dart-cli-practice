00a63b1ce7f69a839666b30a34469fdc500053d7
Nelson0D <nelsonduodu459@gmail.com>
2026-07-31 18:39:47 +0000

    First commit

---
+++ cli/bin/cli.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:command_runner/command_runner.dart';
+
+const version = '0.0.1';
+void main(List<String> arguments) {
+  var commandRunner = CommandRunner(
+    onOutput: (String output) async {
+      await write(output);
+    },
+    onError: (Object error) {
+      if (error is Error) {
+        throw error;
+      }
+      if (error is Exception) {
+        print(error);
+      }
+    },
+  )..addCommand(HelpCommand());
+  commandRunner.run(arguments);
+}
